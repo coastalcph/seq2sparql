7a59acab7195322856bd500fcc911332b81b1c69
Rahul <rahulife007@gmail.com>
2021-05-06 15:07:44 +0200

    new translations

--- translate.py
+++ translate.py
@@ -4,21 +4,24 @@ from google.cloud import translate_v2 as translate
 translate_client = translate.Client()
 
 
-def translate_file(target, split):
-    with open(f"./multicfq/multicfq_{split}.json", "r") as f:
+def translate_file(target):
+    with open(f"./cwq/dataset.json", "r") as f:
         data = json.load(f)
     for idx in range(len(data)):
         data[idx][f"questionWithBrackets_{target}"] = translate_client.translate(
             data[idx][f"questionWithBrackets"], target_language=target
         )["translatedText"]
-    with open(f"./multicfq/multicfq_{split}.json", "w") as f:
+        data[idx][f"questionPatternModEntities_{target}"] = translate_client.translate(
+            data[idx][f"questionPatternModEntities"], target_language=target
+        )["translatedText"]
+    with open(f"./cwq/dataset.json", "w") as f:
         json.dump(data, f)
 
 
 def main():
-    # translate_file("kn", "test")
-    # translate_file("he", "test")
-    translate_file("zh", "test")
+    translate_file("kn")
+    translate_file("he")
+    translate_file("zh")
 
 
 if __name__ == "__main__":
