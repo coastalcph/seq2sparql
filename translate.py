2130764e8d5f1d4c4fb18e05036a35c5cbdbe967
Rahul <rahulife007@gmail.com>
2021-05-07 14:27:50 +0200

    auto-add question mark before translation

--- translate.py
+++ translate.py
@@ -1,19 +1,23 @@
 import json
+
+from tqdm import tqdm
 from google.cloud import translate_v2 as translate
 
 translate_client = translate.Client()
 
 
+def translate_with_qmark(q, target):
+    q = q if q[-1] == "?" else q + "?"
+    q_trans = translate_client.translate(q, target_language=target)["translatedText"]
+    return q_trans[:-1] if q_trans[-1] == "?" else q_trans
+
+
 def translate_file(target):
     with open(f"./cwq/dataset.json", "r") as f:
         data = json.load(f)
-    for idx in range(len(data)):
-        data[idx][f"questionWithBrackets_{target}"] = translate_client.translate(
-            data[idx][f"questionWithBrackets"], target_language=target
-        )["translatedText"]
-        data[idx][f"questionPatternModEntities_{target}"] = translate_client.translate(
-            data[idx][f"questionPatternModEntities"], target_language=target
-        )["translatedText"]
+    for idx in tqdm(range(len(data))):
+        data[idx][f"questionWithBrackets_{target}"] = translate_with_qmark(data[idx][f"questionWithBrackets"], target)
+        data[idx][f"questionPatternModEntities_{target}"] = translate_with_qmark(data[idx][f"questionPatternModEntities"], target)
     with open(f"./cwq/dataset.json", "w") as f:
         json.dump(data, f)
 
