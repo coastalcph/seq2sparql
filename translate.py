e039eeac6678d646c618745c120b93043c620c45
hclent <hclent1@gmail.com>
2021-05-24 14:39:12 +0200

    remoev mod entitiy replacement

--- translate.py
+++ translate.py
@@ -37,31 +37,29 @@ def translate_with_qmark(q, qMod, target):
     assert num_f == num_b
     
     #get the ModEntities in order
-    modEntity_list = []
-    for position, char in enumerate(qMod):
-        if char=="M":
-            if qMod[position+1].isnumeric():
-                if position+2 != len(qMod):
-                    if qMod[position+2] == " ":
-                        modEntity_list.append(qMod[position:position+2])
-                else:
-                    modEntity_list.append(qMod[position:position+2])
+    #modEntity_list = []
+    #for position, char in enumerate(qMod):
+    #    if char=="M":
+    #        if qMod[position+1].isnumeric():
+    #            if position+2 != len(qMod):
+    #                if qMod[position+2] == " ":
+    #                    modEntity_list.append(qMod[position:position+2])
+    #            else:
+    #                modEntity_list.append(qMod[position:position+2])
     try:
         assert len(modEntity_list) == num_f
         #translate
         q_trans = translate_client.translate(qBrackets, target_language=target)["translatedText"]
-        
-        q_trans_me = q_trans
-        #replace bracketed entities with ModEntities from modEntity_list
-        for i in range(0, len(modEntity_list)):
-            front = front_bracket[i]
-            back = back_bracket[i]
-            mod = modEntity_list[i] 
-            start = q_trans_me.find(front)
-            end = q_trans_me.find(back)
-            q_trans_me = f"{q_trans_me[:start]}{mod}{q_trans_me[end+1:]}"      
-        print(f"qBrackets: {qBrackets}")
-        print(f"q_trans_me: {q_trans_me}")
+        q_trans_me = translate_client.translate(qMod, target_language=target)["translatedText"]
+        #for i in range(0, len(modEntity_list)):
+        #    front = front_bracket[i]
+        #    back = back_bracket[i]
+        #    mod = modEntity_list[i] 
+        #    start = q_trans_me.find(front)
+        #    end = q_trans_me.find(back)
+        #    q_trans_me = f"{q_trans_me[:start]}{mod}{q_trans_me[end+1:]}"      
+        #print(f"qBrackets: {qBrackets}")
+        #print(f"q_trans_me: {q_trans_me}")
 
         q_trans_bracket = q_trans[:-1] if q_trans[-1] == "?" else q_trans
         q_trans_me = q_trans_me[:-1] if q_trans_me[-1] == "?" else q_trans_me
